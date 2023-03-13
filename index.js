91c0466bef23c189391f9daca9c4e423cf803044
Mustufa-kerawala <mustufakerawala@outlook.com>
2023-03-13 16:15:55 +0300

    checkin why tests arent working

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
-
 // Write your code here
+function multiply(num1, num2) {
+    num1 = 2;
+    num2 = 31;
+    const res = num1 * num2;
+    return res;
+}
+
+function random() {
+
+    const randomNos = Math.random() * 10;
+    const result = Math.round(randomNos);
+    if(Number.isInteger(result)) {
+        return result;
+    }
+} 
+
+function mod(num3, num4) {
+    num3 = 16;
+    num4 = 6;
+    let res = num3 % num4;
+    return res;
+}
+
+function max() {
+    const maxNos = Math.max(5, 10, 15, 20);
+    return maxNos;
+}
