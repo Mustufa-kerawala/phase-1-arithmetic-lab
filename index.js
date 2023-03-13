df46bdf3592c53b1371292be1278d9180e30817e
Mustufa-kerawala <mustufakerawala@outlook.com>
2023-03-13 19:59:14 +0300

    Debugging

--- index.js
+++ index.js
@@ -7,16 +7,17 @@ function multiply(num1=2, num2=31) {
 function random() {
 
     const randomNos = Math.random() * 10;
-    const random = Math.round(randomNos);
-    if(Number.isInteger(random)) {
+    let random = Math.round(randomNos);
+    if(Number.isInteger(random) && random > 0) {
         return random;
     }
+    else if (random === 0) {
+            return ++random;
+        }
 } 
 
-function mod(num3, num4) {
-    num3 = 16;
-    num4 = 6;
-    let mod = num3 % num4;
+function mod(num3=16, num4=6) {
+    const mod = num3 % num4;
     return mod;
 }
 
