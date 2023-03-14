0a70fb9c4705cfe13908349a809619319257e6d1
Mustufa-kerawala <mustufakerawala@outlook.com>
2023-03-14 13:21:36 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,24 +1,41 @@
 // Write your code here
-function multiply(num1=2, num2=31) {
-    const multiply = num1 * num2;
-    return multiply;
-}
+let num1 = 2;
+let num2 = 31;
+const multiply = num1 * num2;
 
-function random() {
 
-    const randomNos = Math.random() * 10;
-    let random = Math.round(randomNos);
-    if(Number.isInteger(random) && random > 0) {
-        return random;
-    }
-    else if (random === 0) {
-            return ++random;
-        }
-} 
+//  multiply(num1, num2) {
+    
+//     const multiply = num1 * num2;
+//     // return multiply;
+// }
 
-function mod(num3=16, num4=6) {
-    const mod = num3 % num4;
-    return mod;
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 20;
+let num4 = 5;
+let mod = num3 / num4;
+
+
+let max;
+max = Math.max(10, 20);
+
+
+
+// function random() {
+
+//     const randomNos = Math.random() * 10;
+//     let random = Math.round(randomNos);
+//     if(Number.isInteger(random) && random > 0) {
+//         return random;
+//     }
+//     else if (random === 0) {
+//             return ++random;
+//         }
+// } 
+
+function mod(num3=20, num4=5) {
+    const mod = num3 / num4;
 }
 
 function max() {
